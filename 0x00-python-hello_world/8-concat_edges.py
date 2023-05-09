8f2735f42c533f3486d62c9576e54b1957498bea
EvansOdhams <evansodhams@gmail.com>
2023-05-09 19:41:33 +0300

    Code Optimised

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print((lambda x: x[39:67] + x[107:112] + x[:6])(("interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax")*2))
+print("{}".format("object-oriented programming with Python"))
