b11e1a5252b196e650f4adc083a6b4902ca01e88
EvansOdhams <evansodhams@gmail.com>
2023-05-09 18:57:54 +0300

    Print string

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 # Define new variable
 my_string = "Holberton School"
-
 # Print string concatenated three times
 print(3 * my_string)
-
 # Print substring of my_string from index 0 to 8 (inclusive)
 print(my_string[:9])
