72f7d67f5409b624c192afba955f8c523647c2aa
EvansOdhams <evansodhams@gmail.com>
2023-05-09 19:35:44 +0300

    Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+print((lambda x: x[39:67] + x[107:112] + x[:6])(("interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax")*2))
