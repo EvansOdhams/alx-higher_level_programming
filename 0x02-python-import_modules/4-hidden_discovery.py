385561926229f76057ced00a740cd2fa52775e5a
EvansOdhams <evansodhams@gmail.com>
2023-05-11 20:46:53 +0300

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
     import hidden_4
-    
+
     for name in dir(hidden_4):
         if not name.startswith("__"):
             print(name)
