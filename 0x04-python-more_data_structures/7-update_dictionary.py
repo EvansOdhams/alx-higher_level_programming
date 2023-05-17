703b698d743880aabda5befc3a337e1eccefda6b
EvansOdhams <evansodhams@gmail.com>
2023-05-17 20:57:28 +0300

    Add  function that replaces or adds key/value in a dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+a_dictionary = {'language': 'Python', 'version': 3.9}
+update_dictionary(a_dictionary, 'version', 3.10)
+update_dictionary(a_dictionary, 'author', 'Guido van Rossum')
+print(a_dictionary)
