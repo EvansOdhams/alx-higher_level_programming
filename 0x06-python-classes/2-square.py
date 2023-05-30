eac41045acad429cd09007874e4eb070fff8729c
EvansOdhams <evansodhams@gmail.com>
2023-05-30 17:39:37 +0300

    Add size validation to Square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Define the class Square"""
+
+
+class Square:
+    """
+    Square class defines a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except AttributeError as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except AttributeError as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
