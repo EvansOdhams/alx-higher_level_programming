4efc16591fe3f974fca2a14bc672ece03aa72007
EvansOdhams <evansodhams@gmail.com>
2023-06-13 19:03:10 +0300

    Implement solution for "Read file" task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define the class"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints its content to stdout."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read())
