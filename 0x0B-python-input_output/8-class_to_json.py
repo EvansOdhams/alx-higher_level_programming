ef12491b54d1e56564a602e7443e8a1c84c7c88d
EvansOdhams <evansodhams@gmail.com>
2023-06-14 15:54:09 +0300

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 10-class_to_json.py
-# Carlos Barros <1543@holbertonschool.com>
 """ File name : 10-class_to_json.py
 """
 
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 11-student.py
-# Carlos Barros <1543@holbertonschool.com>
 """ File name : 11-student.py
 """
 
