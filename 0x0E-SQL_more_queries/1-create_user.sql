36149fa4628f417a3317b1ee58989ca8bcc11d8a
EvansOdhams <evansodhams@gmail.com>
2023-07-19 23:22:13 +0300

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,8 @@
-sudo mysql -u root
+-- creates the MySQL server user user_0d_1.
+   -- user_0d_1 should have all privileges on your MySQL server
+   -- password set to user_0d_1_pwd
+   -- If the user user_0d_1 already exists, script should not fail
 
--- Now you are in the MySQL shell
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'Diplomas19' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
-EXIT;
