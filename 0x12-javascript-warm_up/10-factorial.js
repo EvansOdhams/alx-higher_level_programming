f9ede7fa99ab311cc096785a8573e42630cf3c5a
EvansOdhams <evansodhams@gmail.com>
2023-08-17 06:20:32 +0300

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-// computes and prints a factorial
 
-function factorial (n) {
-  if ((isNaN(n)) || (n === 1)) {
+function factorial(n) {
+  if (isNaN(n) || n === 0) {
     return 1;
   } else {
     return n * factorial(n - 1);
   }
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+const num = parseInt(process.argv[2]);
+
+console.log(factorial(num));
