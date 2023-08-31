dcfab994b9ee40b2ad3206bf605c5fe7dc3d4542
EvansOdhams <evansodhams@gmail.com>
2023-08-31 22:32:03 +0300

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-#Sends a POST request to the passed URL with data
-curl -sX POST --data "email=evansodhams@gmail.com&subject=I will always be here for PLD" "$1"
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+url="$1"
+email="test@gmail.com"
+subject="I will always be here for PLD"
+
+response=$(curl -s -X POST -d "email=$email&subject=$subject" "$url")
+
+echo "POST params:"
+echo -e "\temail: $email"
+echo -e "\tsubject: $subject"
+echo -e "$response"
