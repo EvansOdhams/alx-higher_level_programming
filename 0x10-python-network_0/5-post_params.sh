1f8f614a76103b8d4e0138ca91df59590c8c5a9e
EvansOdhams <evansodhams@gmail.com>
2023-08-31 23:08:11 +0300

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
+# Sends a GET request to a URL and displays the body of the response
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-response=$(curl -s -X POST -d "email=$email&subject=$subject" "$url")
-
-echo "POST params:"
-echo -e "\temail: $email"
-echo -e "\tsubject: $subject"
-echo -e "$response"
+curl -s "$1" -H "X-School-User-Id: 98"
