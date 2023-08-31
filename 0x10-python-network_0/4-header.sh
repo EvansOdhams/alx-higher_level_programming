815e9acb4b9020d16855c7599ac2754aaa172465
EvansOdhams <evansodhams@gmail.com>
2023-08-31 23:05:09 +0300

    Add new code

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-# script to send custom headers to servers
+# Sends a GET request to a URL and displays the body of the response
 
-url="$1"
-header="X-School-User-Id: 98"
-
-response=$(curl -H "$header" "$url" 2>/dev/null)
-
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to retrieve response from the server."
-else
-    echo "$response"
-fi
+curl -s "$1" -H "X-School-User-Id: 98"
